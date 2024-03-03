cae558c97468e481b713b9dfd85468734a55d054
Frsxk <fazaalbanna0410@gmail.com>
2024-03-03 14:13:43 +0700

    project finished, added blog template

--- main.js
+++ main.js
@@ -1,25 +1,6 @@
 var slides = document.querySelectorAll(`.slide`);
-var buttons = document.querySelectorAll(`.slider-btn`)
+var buttons = document.querySelectorAll(`.slider-btn`);
 let currentSlide = 1;
-
-var manualNav = function(manual) {
-    slides.forEach(function(slide) {
-        slide.classList.remove(`active`);
-
-        buttons.forEach((btn) => {
-            btn.classList.remove(`active`);
-        });
-    });
-
-    slides[manual].classList.add(`active`);
-    buttons[manual].classList.add(`active`);
-}
-buttons.forEach(function(btn, i) {
-    btn.addEventListener(`click`, function() {
-        manualNav(i);
-        currentSlide = i;
-    })
-})
 
 var repeat = function(activeClass) {
     let active = document.getElementsByClassName(`active`);
@@ -43,6 +24,25 @@ var repeat = function(activeClass) {
     repeater();
 }
 repeat();
+
+var manualNav = function(manual) {
+    slides.forEach(function(slide) {
+        slide.classList.remove(`active`);
+
+        buttons.forEach((btn) => {
+            btn.classList.remove(`active`);
+        });
+    });
+
+    slides[manual].classList.add(`active`);
+    buttons[manual].classList.add(`active`);
+}
+buttons.forEach(function(btn, i) {
+    btn.addEventListener(`click`, function() {
+        manualNav(i);
+        currentSlide = i;
+    })
+});
 
 var blogs = document.querySelectorAll(`.blog`);
 blogs.forEach( (p, i) => {
